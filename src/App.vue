63bc87b8c85fd14e83bb56168652ddb5ab77dabd
김한들 <dle1883@gmail.com>
2025-12-13 14:20:16 +0900

    commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>test 입니다</h1>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+        data() {
+            return {}
+        }
+    }
+</script>
+<style></style>
